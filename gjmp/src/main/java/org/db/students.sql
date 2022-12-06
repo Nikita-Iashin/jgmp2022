a832c710149ffcf2434605b36e38908182290115
Nikita Iashin <nikita_iashin@epam.com>
2022-12-06 08:59:14 +0200

    add some sort tests

--- gjmp/src/main/java/org/db/students.sql
+++ gjmp/src/main/java/org/db/students.sql
@@ -49,13 +49,48 @@ CREATE TABLE students_subject
     UNIQUE (student_id, subject_id)
 );
 
+CREATE TABLE exam_results
+(
+    exam_id      SERIAL,
+    subject_name text,
+    score        int,
+    student_id   integer REFERENCES students (id)
+);
+
+insert into exam_results (subject_name, score, student_id)
+VALUES ('Math', 3, 1);
+insert into exam_results (subject_name, score, student_id)
+VALUES ('History', 2, 2);
+insert into exam_results (subject_name, score, student_id)
+VALUES ('Math', 1, 3);
+insert into exam_results (subject_name, score, student_id)
+VALUES ('Math', 4, 4);
+
+
+--Try different kind of indexes (B-tree, Hash, GIN, GIST) for your fields.
 insert into students_subject (student_id, subject_id) values  (1,1);
 insert into students_subject (student_id, subject_id) values  (1,2);
 insert into students_subject (student_id, subject_id) values  (2,2);
+--Analyze performance for each of the indexes (use ANALYZE and EXPLAIN). Check the size of the index.
+
+--Try to set index before inserting test data and after. What was the time? Test data:
+  --a. 100K of users
+  --b. 1K of subjects
+  --c. 1 million of marks
+  --Test queries:
+  --a. Find user by name (exact match)
+  --b. Find user by surname (partial match)
+  --c. Find user by phone number (partial match)
+  --d. Find user with marks by user surname (partial match)
+  --Add your investigations to separate document. (1 point)
 
 create index names_students on students USING btree (id);
 create index id_subjects on subjects USING hash (id);
 create index id_students on students_subject (id);
+
+--TODO
+
+--  5. Add trigger that will update column updated_datetime to current date in case of updating any of student. (0.3 point)
 
 CREATE FUNCTION update_student() RETURNS TRIGGER AS $$
 BEGIN
@@ -69,9 +104,30 @@ CREATE TRIGGER update_student
     FOR ROW
     EXECUTE PROCEDURE update_student();
 
+-- 6. Add validation on DB level that will check username on special characters
+--(reject student name with next characters '@', '#', '$'). (0.3 point)
+
 alter table students
     add constraint check_name
     check (Name ~* '^[^@#$]+$');
+
+--7. Create snapshot that will contain next data: student name, student surname, subject name, mark
+--(snapshot means that in case of changing some data in source table – your snapshot should not change). (0.3 point)
+--todo
+--8. Create function that will return average mark for input user. (0.3 point)
+--9. Create function that will return avarage mark for input subject name. (0.3 point).
+--todo
+--10. Create function that will return student at "red zone" (red zone means at least 2 marks <=3). (0.3 point)
+create or replace function redZone21()
+    returns table (name text)
+AS
+$$
+BEGIN
+    RETURN QUERY
+    SELECT s.name FROM students s INNER JOIN exam_results er on s.id = er.student_id;
+END;
+$$
+    LANGUAGE plpgsql;
 
 
 
