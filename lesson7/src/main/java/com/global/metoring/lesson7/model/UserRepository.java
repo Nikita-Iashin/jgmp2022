23db2423c0a223fbc8697b6f52ede9c04dcbeab9
Nikita Iashin <nikita_iashin@epam.com>
2023-01-19 08:40:16 +0200

    mvc started

---
+++ lesson7/src/main/java/com/global/metoring/lesson7/model/UserRepository.java
@@ -0,0 +1,7 @@
+package com.global.metoring.lesson7.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
---
+++ lesson7/src/test/java/com/global/metoring/lesson7/controller/UserControllerTest.java
@@ -0,0 +1,17 @@
+package com.global.metoring.lesson7.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    UserController userController;
+    @Test
+    void testDbConnection() {
+    }
+}
