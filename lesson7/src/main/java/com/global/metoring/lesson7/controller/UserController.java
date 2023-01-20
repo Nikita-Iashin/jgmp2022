9b702dda4c013ef0adb69d1a4badc279c9525ea8
Nikita Iashin <nikita_iashin@epam.com>
2023-01-20 06:26:29 +0200

    finished except bookings from XML file

--- lesson7/src/main/java/com/global/metoring/lesson7/controller/UserController.java
+++ lesson7/src/main/java/com/global/metoring/lesson7/controller/UserController.java
@@ -2,6 +2,7 @@ package com.global.metoring.lesson7.controller;
 
 import com.global.metoring.lesson7.model.User;
 import com.global.metoring.lesson7.model.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 @RequestMapping(path = "/users")
 public class UserController {
 
@@ -21,12 +23,14 @@ public class UserController {
 
     @GetMapping("/index")
     public String showUserList(Model model) {
+        log.info("Getting all users");
         model.addAttribute("users", userRepository.findAll());
         return "index";
     }
 
     @PostMapping("/update/{id}")
     public String updateUser(@PathVariable("id") Integer id, @Validated User user, BindingResult result, Model model) {
+        log.info("Creating user");
         if (result.hasErrors()) {
             user.setId(id);
             return "update-user";
@@ -37,6 +41,7 @@ public class UserController {
 
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
+        log.info("Deleting user");
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
--- lesson7/src/test/java/com/global/metoring/lesson7/controller/UserControllerTest.java
+++ lesson7/src/test/java/com/global/metoring/lesson7/controller/UserControllerTest.java
@@ -1,17 +1,41 @@
 package com.global.metoring.lesson7.controller;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
-
     @Autowired
-    UserController userController;
+    private MockMvc mockMvc;
+
     @Test
-    void testDbConnection() {
+    @SneakyThrows
+    public void testShouldCheckGetAllUsersRequest() {
+        mockMvc
+                .perform(get("http://localhost:8080/users/all"))
+                .andDo(print())
+                .andExpect(status()
+                        .isOk());
     }
+
+    @Test
+    @SneakyThrows
+    public void testShouldCheckPostUserRequest() {
+        mockMvc
+                .perform(post("http://localhost:8080/users/add"))
+                .andDo(print())
+                .andExpect(status()
+                        .isOk());
+    }
+
 }
